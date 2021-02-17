8dc62bdb50c391239c47ca7c544ce9fb8fcccbc6
rezavai92 <nijhumreza52@gmail.com>
2021-02-18 01:05:13 +0600

    color changer added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ window.addEventListener('load',()=>{
     canvas.height = window.innerHeight;
     const ctx= canvas.getContext("2d")
    
+    ctx.strokeStyle='black'
+    
+    
     let paint = false;
     function start(e){
 
@@ -21,23 +24,32 @@ window.addEventListener('load',()=>{
         if(!paint) return;
         ctx.lineCap="round";
         ctx.lineWidth=10;
-        ctx.strokeStyle='red'
+        
         ctx.lineTo(e.clientX,e.clientY);
         ctx.stroke()
         ctx.beginPath()
         ctx.moveTo(e.clientX,e.clientY)
     }
 
-    window.addEventListener("mousedown",(e)=>{
+    function changeColor(e){
+        ctx.strokeStyle=e.target.value;
+    }
+
+    const colorChanger=document.querySelector("#changeColorInp")
+    colorChanger.addEventListener("change",(e)=>{
+        changeColor(e)
+    })
+
+    canvas.addEventListener("mousedown",(e)=>{
 
         start(e)
 
     })
-    window.addEventListener("mouseup",()=>{
+    canvas.addEventListener("mouseup",()=>{
 
         stop()
     })
-    window.addEventListener("mousemove",(e)=>{
+    canvas.addEventListener("mousemove",(e)=>{
 
         draw(e)
     })
