dcbf0f65191d4393622df9573b6634399a381941
rezavai92 <nijhumreza52@gmail.com>
2021-02-18 13:45:16 +0600

    different font size added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.addEventListener('load',()=>{
     canvas.width = window.innerWidth;     // equals window dimension
     canvas.height = window.innerHeight;
     const ctx= canvas.getContext("2d")
-   
+    ctx.lineWidth=3;
     ctx.strokeStyle='black'
     
     
@@ -23,7 +23,7 @@ window.addEventListener('load',()=>{
 
         if(!paint) return;
         ctx.lineCap="round";
-        ctx.lineWidth=10;
+       
         
         ctx.lineTo(e.clientX,e.clientY);
         ctx.stroke()
@@ -31,6 +31,56 @@ window.addEventListener('load',()=>{
         ctx.moveTo(e.clientX,e.clientY)
     }
 
+    function changeFontWeight(level){
+
+        ctx.lineWidth=level;
+    }
+
+    const brush = document.querySelector(".paint-brush");
+    
+    brush.addEventListener( "click",(e)=>{
+        const pop = document.querySelector(".popover");
+        pop.style.visibility=pop.style.visibility=="visible"?"hidden":"visible"
+        if(pop.style.visibility=="visible"){
+            paint=false;
+
+        }
+        
+    })
+//x1 button
+    const x1= document.querySelector('.x1-btn');
+    x1.addEventListener('click',(e)=>{
+        
+        changeFontWeight(3);
+        console.log("hello")
+    })
+
+//x2 button
+    const x2= document.querySelector('.x2-btn');
+    x2.addEventListener('click',(e)=>{
+        
+        changeFontWeight(5);
+        console.log("hello")
+    })
+
+//x3 button
+    const x3= document.querySelector('.x3-btn');
+    x3.addEventListener('click',(e)=>{
+        
+        changeFontWeight(10);
+        console.log("hello")
+    })
+
+    //click outside toolbar , inside canvas
+    canvas.addEventListener("click",()=>{
+
+        const pop = document.querySelector(".popover");
+        pop.style.visibility="hidden"
+        
+    })
+
+
+   
     function changeColor(e){
         ctx.strokeStyle=e.target.value;
     }
@@ -42,8 +92,15 @@ window.addEventListener('load',()=>{
 
     canvas.addEventListener("mousedown",(e)=>{
 
-        start(e)
+        
+        const pop = document.querySelector(".popover");
+        console.log(pop.style.visibility)
+        if(pop.style.visibility=="visible"){
 
+        }
+        else{
+            start(e)
+        }
     })
     canvas.addEventListener("mouseup",()=>{
 
